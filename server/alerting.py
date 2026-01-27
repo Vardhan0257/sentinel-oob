18a899a5cb751143eea08ca104440f5275443cad
M Indraneeli Vardhan <vardhanm0257@gmail.com>
2026-01-27 21:04:02 +0530

    Define alerting interface

---
+++ server/alerting.py
@@ -0,0 +1,15 @@
+"""
+Sentinel-OOB Alerting Interface (v0.1)
+
+Defines how out-of-band alerts are sent.
+No implementation yet.
+"""
+
+def send_alert(message: str):
+    """
+    Send an out-of-band alert.
+
+    Args:
+        message (str): Alert message payload
+    """
+    pass
